041eb56a3aac743c55ed19c51d7d0224703ab402
hzhuyuanyuan <hzhuyuanyuan@corp.netease.com>
2018-04-28 15:38:19 +0800

    新的工程

---
+++ src/main/java/com/yanxuan/test/TestReportApplication.java
@@ -0,0 +1,12 @@
+package com.yanxuan.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestReportApplication.class, args);
+	}
+}
